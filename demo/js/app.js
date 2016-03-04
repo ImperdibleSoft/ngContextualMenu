3ff959171c51c3716a2833618dfda3f05ff1a12b
Rafael Pérez García <rperezgarcia@devspark.com>
2016-03-04 17:19:47 -0300

    Readme and some improvements

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute', 'ngConsole']);
+var app = angular.module('app', ['ngRoute', 'ngContextualMenu']);
 
 app.config(['$routeProvider', function($routeProvider){
   $routeProvider
